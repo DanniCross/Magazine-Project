d8ad14dedefd382cd1a2133d4763cc5b3f8b7f69
Camilog699 <amraga10@gmail.com>
2019-08-09 16:23:08 -0500

    users

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { NgxCaptchaModule } from 'ngx-captcha';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './landing/header/header.component';
-import { HomeComponent } from './landing/home/home.component';
-import { FooterComponent } from './landing/footer/footer.component';
-import { CarouselComponent } from './landing/carousel/carousel.component';
-import { TemporalComponent } from './landing/temporal/temporal.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./landing/header/header.component";
+import { HomeComponent } from "./landing/home/home.component";
+import { FooterComponent } from "./landing/footer/footer.component";
+import { CarouselComponent } from "./landing/carousel/carousel.component";
+import { TemporalComponent } from "./landing/temporal/temporal.component";
 
 @NgModule({
   declarations: [
@@ -18,11 +19,8 @@ import { TemporalComponent } from './landing/temporal/temporal.component';
     CarouselComponent,
     TemporalComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, NgxCaptchaModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  captchakey: '6Le9VLIUAAAAAMqgjRxesODLF4T9fto4akoM6c8_'
 };
 
 /*
