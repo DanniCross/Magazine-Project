cec85cd373a7658e32fe5c758f94fdfc4725a339
Jose Cruz <josedanielcrzgmail.com>
2019-08-10 16:01:22 -0500

    AutorModule

--- client/src/app/modules/autor/home-autor/home-autor.component.ts
+++ client/src/app/modules/autor/home-autor/home-autor.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
 import { UserService } from 'src/app/services/users/user.service';
 import { isNullOrUndefined } from 'util';
 
@@ -7,17 +7,24 @@ import { isNullOrUndefined } from 'util';
   templateUrl: './home-autor.component.html',
   styleUrls: ['./home-autor.component.css']
 })
-export class HomeAutorComponent implements OnInit {
+export class HomeAutorComponent implements OnInit, DoCheck {
 
   constructor(private user: UserService) { }
 
   ngOnInit() {
+    this.Logged();
+  }
+
+  ngDoCheck() {
+    this.Logged();
+  }
+
+  username = '';
+
+  Logged() {
     let user = this.user.getUserInfo();
     if (!isNullOrUndefined(user)) {
       this.username = `${user['name']} ${user['lastname']}`;
     }
   }
-
-  username = '';
-
 }
