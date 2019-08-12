cd0498745895581d3d0029ac824fb28ee9debb56
Jose Cruz <josedanielcrzgmail.com>
2019-08-12 00:24:14 -0500

    Email method

--- common/models/article.js
+++ common/models/article.js
@@ -1,5 +1,54 @@
 'use strict';
 
-module.exports = function(Article) {
+module.exports = function (Article) {
+    Article.sendEmail = (sender, message, subject, emailAdresses, cb) => {
+        Article.app.models.Email.send({
+            to: emailAdresses,
+            from: sender,
+            subject: subject,
+            text: message,
+            html: message
+        }, function (err, mail) {
+            console.log("Email sended...");
+            if (err) return err;
+        });
+        cb(null, 'message sent:' + message);
+    }
 
+    Article.remoteMethod('sendEmail', {
+        http: {
+            path: '/sendEmail',
+            verb: 'get'
+        },
+        description: [
+            "Api to send email messages."
+        ],
+        accepts: [
+            {
+                arg: 'sender',
+                type: 'string',
+                required: true
+            },
+            {
+                arg: 'message',
+                type: 'string',
+                required: true
+            },
+            {
+                arg: 'subject',
+                type: 'string',
+                required: true
+            },
+            {
+                arg: 'emailAdresses',
+                type: 'string',
+                required: true
+            }
+        ],
+        returns: {
+            arg: 'Email',
+            type: 'string'
+        }
+
+    });
 };
